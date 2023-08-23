980a5559525cb81989c9db9cd1e2ae477b76795d
Norma Enriquez <normalety455@gmail.com>
2023-08-23 11:11:57 -0600

    agregando boton

--- index.js
+++ index.js
@@ -1,44 +1,47 @@
 document.addEventListener('DOMContentLoaded', () => {
-  let api = `https://reqres.in/api/users?delay=3`;
+    let api = `https://reqres.in/api/users?delay=3`;
 
-  const checkExpirationAndFetchData = () => {
-      const storedData = localStorage.getItem('userData');
-      const storedExpirationTime = localStorage.getItem('expirationTime');
-      const currentTime = Date.now();
+    const fetchData = () => {
+        const getDataButton = document.getElementById('getDataButton');
+        const loadingText = document.getElementById('loadingText');
 
-      if (storedData && storedExpirationTime && currentTime < parseInt(storedExpirationTime)) {
-          mostrarData(JSON.parse(storedData));
-      } else {
-          fetchData();
-      }
-  };
+        getDataButton.disabled = true; // Deshabilitar el botón durante la carga
+        loadingText.style.display = 'block'; // Mostrar el texto de carga
 
-  const fetchData = () => {
-      fetch(api)
-          .then(response => response.json())
-          .then(data => {
-              const expirationTime = Date.now() + 60000; 
-              localStorage.setItem('userData', JSON.stringify(data));
-              localStorage.setItem('expirationTime', expirationTime.toString());
-              mostrarData(data);
-          })
-          .catch(error => console.log(error));
-  };
+        fetch(api)
+            .then(response => response.json())
+            .then(data => {
+                const expirationTime = Date.now() + 60000; 
+                const currentTime = new Date().toISOString();
+                const dataWithTimestamp = {
+                    timestamp: currentTime,
+                    data: data.data
+                };
+                localStorage.setItem('userData', JSON.stringify(dataWithTimestamp));
+                localStorage.setItem('expirationTime', expirationTime.toString());
+                mostrarData(dataWithTimestamp);
 
-  const mostrarData = (data) => {
-      let body = '';
-      for (let i = 0; i < data.data.length; i++) {
-          body += `
-              <tr>
-                  <td>${data.data[i].id}</td>
-                  <td>${data.data[i].email}</td>
-                  <td>${data.data[i].first_name}</td>
-                  <td>${data.data[i].last_name}</td>
-                  <td><img src="${data.data[i].avatar}" alt="Avatar"></td>
-              </tr>`;
-      }
-      document.getElementById('data').innerHTML = body;
+                getDataButton.disabled = false; // Habilitar el botón
+                loadingText.style.display = 'none'; // Ocultar el texto de carga
+            })
+            .catch(error => console.log(error));
     };
 
-    checkExpirationAndFetchData();
+    const mostrarData = (data) => {
+        let body = '';
+        for (let i = 0; i < data.data.length; i++) {
+            body += `
+                <tr>
+                    <td>${data.data[i].id}</td>
+                    <td>${data.data[i].email}</td>
+                    <td>${data.data[i].first_name}</td>
+                    <td>${data.data[i].last_name}</td>
+                    <td><img src="${data.data[i].avatar}" alt="Avatar"></td>
+                </tr>`;
+        }
+        document.getElementById('data').innerHTML = body;
+    };
+
+    const button = document.getElementById('getDataButton');
+    button.addEventListener('click', fetchData);
 });
